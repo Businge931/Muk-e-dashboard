bb9b28222379e70f3130984b836e655beb7a7a2b
Businge931 <jimmybusinge9@gmail.com>
2022-10-16 01:13:56 +0300

    adjusting position

---
+++ src/Components/Updates/Updates.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from "./Updates.module.css";
+const Updates = () => {
+  return (
+    <div className={classes.updates}>
+      <p className={classes.updates_text}>Updates</p>
+    </div>
+  );
+};
+
+export default Updates;
