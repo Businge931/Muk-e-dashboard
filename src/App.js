d2814b3e9dd115b247c65d9c3e29517439532d2e
Businge931 <jimmybusinge9@gmail.com>
2022-10-16 00:23:21 +0300

    root file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import classes from "./App.module.css";
+import AllEvents from "./Components/All Events/AllEvents";
+import Header from "./Components/Header/Header";
+import EventContent from "./Components/Middle section/EventContent";
+import Notifications from "./Components/Notifications/Notifications.jsx";
+import Users from "./Components/Users/Users";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <AllEvents />
+      <div className={classes.root_right}>
+        <Header />
+        <div className={classes.root_content}>
+          <EventContent />
+          <div className={classes.root_content__aside}>
+            <Notifications />
+            <Users />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
